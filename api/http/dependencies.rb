94e00901165427016edee9e31091c9e94c47bee8
raventid <juliankul@gmail.com>
2017-01-22 16:50:55 +0300

    register dependencies before load code

--- api/http/dependencies.rb
+++ api/http/dependencies.rb
@@ -14,48 +14,56 @@ class Container
   extend Dry::Container::Mixin
 end
 
-
-# Possible bidirectional algorithms
-require_relative '../../lib/algorithms/revertable/bijective'
-# Possible one-direction algorithms
-require_relative '../../lib/algorithms/hashing/random_string'
-Container.register "algorithm", -> { Shortener::Algorithm::RandomString.new }
-Import = Dry::AutoInject(Container)
-
+Container.register("algorithm") do
+  # Possible bidirectional algorithms
+  require_relative '../../lib/algorithms/revertable/bijective'
+  # Possible one-direction algorithms
+  require_relative '../../lib/algorithms/hashing/random_string'
+  Shortener::Algorithm::RandomString.new 
+end
 
 # Storage adapters
-require_relative '../../persistence/adapters/no_storage'
-require_relative '../../persistence/adapters/hiredis'
-Container.register "adapter", -> { Shortener::Persistence::MHiredis.new }
-Import = Dry::AutoInject(Container)
-
+Container.register("adapter") do
+  require_relative '../../persistence/adapters/no_storage'
+  require_relative '../../persistence/adapters/hiredis'
+  Shortener::Persistence::MHiredis.new
+end
 
 # Storages 
-require_relative '../../persistence/storage'
-Container.register "storage", -> { Shortener::Persistence::Storage.new }
-Import = Dry::AutoInject(Container)
-
+Container.register("storage") do
+  require_relative '../../persistence/storage'
+  Shortener::Persistence::Storage.new
+end
 
 # Validation
-require_relative '../../operations/url/validations/validate'
-Container.register "validate", -> { Shortener::Operation::Validate.new }
-Import = Dry::AutoInject(Container)
-
+Container.register("validate") do
+  require_relative '../../operations/url/validations/validate'
+  Shortener::Operation::Validate.new 
+end
 
 # Operations
-require_relative '../../operations/url/encode_or_save'
-require_relative '../../operations/url/decode_or_load'
-Container.register "encode_or_save", -> { Shortener::Operation::EncodeOrSave.new }
-Container.register "decode_or_load", -> { Shortener::Operation::DecodeOrLoad.new }
-Import = Dry::AutoInject(Container)
+Container.register("encode_or_save") do
+  require_relative '../../operations/url/encode_or_save'
+  Shortener::Operation::EncodeOrSave.new
+end
 
-require_relative '../../operations/url/create'
-require_relative '../../operations/url/read'
-Container.register "create", -> { Shortener::Operation::Create.new }
-Container.register "read", -> { Shortener::Operation::Read.new }
-Import = Dry::AutoInject(Container)
+Container.register("decode_or_load") do
+  require_relative '../../operations/url/decode_or_load'
+  Shortener::Operation::DecodeOrLoad.new
+end
 
+
+Container.register("create") do
+  require_relative '../../operations/url/create'
+  Shortener::Operation::Create.new
+end
+
+
+Container.register("read") do
+  require_relative '../../operations/url/read'
+  Shortener::Operation::Read.new
+end
 
 # Set up an AutoInject to use our container
 Import = Dry::AutoInject(Container)
-
+ 
